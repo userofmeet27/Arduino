1a2400a573f427808c1d8b3401cd6c3c7092c704
Meet Jain <meetjain538@gmail.com>
2024-02-05 18:34:49 +0530

    Update LCD Print.ino

--- LCD Print.ino
+++ LCD Print.ino
@@ -1,15 +1,11 @@
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
-
 LiquidCrystal_I2C lcd(0x27, 16, 2); 
-
 void lcd_print(String txt){
   txt.trim();
   lcd.clear(); 
   lcd.setCursor(0, 0);
-
   for (int i = 0; i < txt.length(); i++){
-
     if (i == 16){ 
       lcd.setCursor(0, 1);
       if (txt[i] == ' '){
@@ -19,19 +15,14 @@ void lcd_print(String txt){
     else if (i == 33){
       break;
     }
-
     lcd.print(txt[i]);
   }
 }
-
 void setup(){
   Serial.begin(9600);
   lcd.init();     
   lcd.backlight(); 
 }
-
-
-
 void loop(){
   while (!Serial.available()){
     delay(10);
